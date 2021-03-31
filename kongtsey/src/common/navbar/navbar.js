6bc692dc69dc6279dd79a419f78c1fc88c409bcb
phuntsho <norbu_phuntsho@wheatoncollege.edu>
2021-04-01 01:11:19 +0600

    IN PROGRESS: still working on the navbar drawer

--- kongtsey/src/common/navbar/navbar.js
+++ kongtsey/src/common/navbar/navbar.js
@@ -1,25 +1,36 @@
 import React from 'react';
-import { Grid } from '@material-ui/core';
-import List from '@material-ui/core/List';
-import ListItem from '@material-ui/core/ListItem';
-import ListItemText from '@material-ui/core/ListItemText';
+import { Grid, List, ListItem, ListItemText, ListItemIcon } from '@material-ui/core';
 import Logo from '../../asset/logo.png';
-// import styles from './navbar.module.scss';
+import EmailIcon from '@material-ui/icons/Email';
+import styles from './navbar.module.scss';
 
 function Navbar(props) {
     // const [open, setOpen] = React.useState(true);
     // const [openExp, setOpenExp] = React.useState(true);
 
     return (
+        // TODO: need to center the text and add icons as well.
         <Grid container spacing={0} className={'parent-container'}>
             <Grid item xs={12} md={12} lg={12} sm={12}>
                 <List>
-                    <ListItem button key={'logo'}>
+                    {/* company logo */}
+                    <ListItem button key={'logo'} className={styles.listItem}>
                         <img src={Logo} alt="company logo" style={{ width: '106%' }} />
                     </ListItem>
-
-                    <ListItem button key={'donate'}>
-                        <ListItemText primary={'donate'} />
+                    <ListItem button key={'contact'} className={styles.listItem}>
+                        <ListItemText primary={'Contact'} style={{ textAlign: 'center' }} />
+                    </ListItem>
+                    <ListItem button key={'about'} className={styles.listItem}>
+                        <ListItemText primary={'About'} style={{ textAlign: 'center' }} />
+                    </ListItem>
+                    <ListItem button key={'donate'} className={styles.listItem}>
+                        <ListItemText primary={'Donate'} style={{ textAlign: 'center' }} />
+                    </ListItem>
+                    <ListItem button key={'login'} className={styles.listItem}>
+                        <ListItemText primary={'Login'} style={{ textAlign: 'center' }} />
+                    </ListItem>
+                    <ListItem button key={'sign-up'} className={styles.listItem}>
+                        <ListItemText primary={'Sign Up'} style={{ textAlign: 'center' }} />
                     </ListItem>
                 </List>
             </Grid>
